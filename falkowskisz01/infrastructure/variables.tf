0c1a7689ec31edbee702b9dec91f2b40698dce4c
Szymon <falkowskisz01@gmail.com>
2026-03-04 17:04:04 +0100

    feat: szymon falkowski initial commit

---
+++ falkowskisz01/infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  type        = string
+  description = "GCP Project ID"
+  default     = "project-f5f4f6f0-acae-485b-a16"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP Region"
+  default     = "europe-central2"
+}
