1f53635733ac22ea415505a5b80219931d5c98b7
Lukasz Blok <318094@stud.umk.pl>
2026-03-11 10:30:45 +0100

    Add hello-lukasz terraform infrastructure

---
+++ lab/tydzien2/lublok/infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+  default     = "project-f5f4f6f0-acae-485b-a16"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "europe-central2"
+}
