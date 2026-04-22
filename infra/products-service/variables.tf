be87c80d3dd029a471b30b78ff04e3bd24bb0ac5
Krzysztof Muchewicz <krzysztof@muchewicz.pl>
2026-04-22 14:54:34 +0200

    workflow and infra for product service

---
+++ infra/products-service/variables.tf
@@ -0,0 +1,22 @@
+variable "project" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "project-f5f4f6f0-acae-485b-a16"
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "europe-central2"
+}
+
+variable "service_name" {
+  description = "Cloud Run service name"
+  type        = string
+  default     = "products-service-dev"
+}
+
+variable "image" {
+  description = "Docker image to deploy"
+  type        = string
+}
