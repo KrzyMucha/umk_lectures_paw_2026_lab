87a62d2cf35044c957d3752597103770705a6214
szymonorz <originalemail001@gmail.com>
2026-04-22 16:35:50 +0200

    fix: memory limits

--- infra/offers-service/main.tf
+++ infra/offers-service/main.tf
@@ -29,7 +29,7 @@ resource "google_cloud_run_v2_service" "offers_service" {
       resources {
         limits = {
           cpu    = "1"
-          memory = "256Mi"
+          memory = "512Mi"
         }
         startup_cpu_boost = true
       }
