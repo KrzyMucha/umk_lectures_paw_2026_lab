f3a88832416bd7c4e02b693b9457f68c1d057c04
Lukasz Blok <318094@stud.umk.pl>
2026-04-22 15:03:46 +0200

    feat(review): add product-review-service skeleton with hardcoded data

---
+++ infra/dev/product-review-service.tf
@@ -0,0 +1,44 @@
+resource "google_cloud_run_v2_service" "product_review_service" {
+  name     = "product-review-service-dev"
+  location = var.region
+
+  template {
+    containers {
+      image = "${var.region}-docker.pkg.dev/${var.project}/mini-allegro/product-review-service:latest"
+
+      ports {
+        container_port = 8080
+      }
+
+      resources {
+        limits = {
+          cpu    = "1"
+          memory = "512Mi"
+        }
+        startup_cpu_boost = true
+      }
+
+      env {
+        name  = "APP_ENV"
+        value = "dev"
+      }
+    }
+
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 5
+    }
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "product_review_service_public" {
+  location = google_cloud_run_v2_service.product_review_service.location
+  name     = google_cloud_run_v2_service.product_review_service.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
