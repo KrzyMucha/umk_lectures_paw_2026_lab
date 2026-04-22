10bce3158d92f1163f3cb7edacd2cf0d7209e56f
szymonorz <originalemail001@gmail.com>
2026-04-22 15:27:30 +0200

    chore: refactor

--- infra/main.tf
+++ infra/main.tf
@@ -159,51 +159,6 @@ resource "google_artifact_registry_repository_iam_member" "cloudbuild_writer" {
   member     = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
-resource "google_cloud_run_v2_service" "offers_service" {
-  name     = var.offers_service_name
-  location = var.region
-
-  template {
-    containers {
-      image = "${var.region}-docker.pkg.dev/${var.project}/${google_artifact_registry_repository.mini_allegro.repository_id}/${var.offers_service_name}:latest"
-
-      ports {
-        container_port = 8082
-      }
-
-      resources {
-        limits = {
-          cpu    = "1"
-          memory = "256Mi"
-        }
-        startup_cpu_boost = true
-      }
-
-      env {
-        name  = "PORT"
-        value = "8082"
-      }
-    }
-
-    scaling {
-      min_instance_count = 0
-      max_instance_count = 10
-    }
-  }
-
-  traffic {
-    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-    percent = 100
-  }
-}
-
-resource "google_cloud_run_v2_service_iam_member" "offers_service_public_access" {
-  location = google_cloud_run_v2_service.offers_service.location
-  name     = google_cloud_run_v2_service.offers_service.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
 resource "google_cloud_run_v2_service_iam_member" "public_access" {
   location = google_cloud_run_v2_service.mini_allegro.location
   name     = google_cloud_run_v2_service.mini_allegro.name
--- services/symphony-monolith/src/Controller/OfferController.php
+++ services/symphony-monolith/src/Controller/OfferController.php
@@ -2,94 +2,42 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
+use App\Service\OffersService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/offers')]
 class OfferController extends AbstractController
 {
-    private string $offersServiceUrl;
-
     public function __construct(
-        private readonly LoggerInterface $logger,
-    ) {
-        $this->offersServiceUrl = rtrim($_ENV['OFFERS_SERVICE_URL'] ?? 'http://offers-service:8082', '/');
-    }
+        private readonly OffersService $offersService,
+    ) {}
 
     #[Route('', methods: ['GET'])]
     #[Route('/', methods: ['GET'])]
     public function index(): JsonResponse
     {
-        return $this->proxy('GET', '/offers');
+        return $this->offersService->getOffers();
     }
 
     #[Route('-super', name: 'offers_super', methods: ['GET'])]
     public function super(): JsonResponse
     {
-        return $this->proxy('GET', '/offers-super');
+        return $this->offersService->getSuperOffers();
     }
 
     #[Route('-super', name: 'offers_super_patch', methods: ['PATCH'])]
     public function assignSuperSeller(Request $request): JsonResponse
     {
-        return $this->proxy('PATCH', '/offers-super', $request->getContent());
+        return $this->offersService->assignSuperSeller($request->getContent());
     }
 
     #[Route('', methods: ['POST'])]
     #[Route('/', methods: ['POST'])]
     public function create(Request $request): JsonResponse
     {
-        return $this->proxy('POST', '/offers', $request->getContent());
-    }
-
-    private function proxy(string $method, string $path, ?string $body = null): JsonResponse
-    {
-        $url = $this->offersServiceUrl . $path;
-
-        $opts = [
-            'http' => [
-                'method' => $method,
-                'header' => "Content-Type: application/json\r\nAccept: application/json\r\n",
-                'ignore_errors' => true,
-                'timeout' => 10,
-            ],
-        ];
-
-        if ($body !== null) {
-            $opts['http']['content'] = $body;
-        }
-
-        $context = stream_context_create($opts);
-        $response = @file_get_contents($url, false, $context);
-
-        if ($response === false) {
-            $this->logger->error('Failed to proxy request to offers-service', [
-                'url' => $url,
-                'method' => $method,
-            ]);
-            return new JsonResponse(
-                ['error' => 'Offers service unavailable'],
-                Response::HTTP_BAD_GATEWAY
-            );
-        }
-
-        $statusCode = $this->extractStatusCode($http_response_header ?? []);
-        $data = json_decode($response, true);
-
-        return new JsonResponse($data, $statusCode);
-    }
-
-    private function extractStatusCode(array $headers): int
-    {
-        foreach ($headers as $header) {
-            if (preg_match('/^HTTP\/\S+\s+(\d{3})/', $header, $matches)) {
-                return (int) $matches[1];
-            }
-        }
-        return Response::HTTP_BAD_GATEWAY;
+        return $this->offersService->createOffer($request->getContent());
     }
 }
