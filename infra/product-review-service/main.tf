f6c53979e0da256ebd640e95a14fa35b1c625967
Szymon Falkowski <36602612+falkusowy1@users.noreply.github.com>
2026-05-27 17:27:07 +0200

    feat: znowu naprawiam

--- infra/product-review-service/main.tf
+++ infra/product-review-service/main.tf
@@ -14,7 +14,6 @@ provider "google" {
   region  = var.region
 }
 
-}
 
 # Cloud Run domyslnie uzywa service account: PROJECT_NUMBER-compute@developer.gserviceaccount.com
 # Nadajemy mu dostep do Firestore
@@ -26,7 +25,6 @@ resource "google_project_iam_member" "cloud_run_firestore" {
   member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
 }
 
-}
 
 resource "google_cloud_run_v2_service" "product_review_service" {
   name     = var.service_name
